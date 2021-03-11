e3d284025e88fffcad35d10927b5b85487167755
lingar <yimprogramming@gmail.com>
2021-03-11 22:22:49 +0200

    Small fix

--- src/run/RunMe.java
+++ src/run/RunMe.java
@@ -9,12 +9,12 @@ public class RunMe {
 		//reseting the efficiency counter
 		PrintAllCombinations.counter1 = 0;
 		//Run here the solution
-		String sequence = "123456789";
+		String sequence = "1234";
 		System.out.println("Finding all possible combination from the string: " + sequence);
 		PrintAllCombinations.try1FindAllPossible(new StringBuilder(), new StringBuilder(sequence));
 		System.out.println("Efficiency counter = " + PrintAllCombinations.counter1);
 		long endTime = ( System.nanoTime() - startingTime)/ 1_000_000;
-		System.out.println("It's took about " + endTime + " ms, and " + endTime / 1000 + " seconds");
+		System.out.println("It's took about " + endTime + " ms,  " + endTime / 1000 + " seconds");
 
 		
 	}

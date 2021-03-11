3c2973a0196dacf914a2725ee5c32d576959483b
lingar <yimprogramming@gmail.com>
2021-03-11 20:11:16 +0200

    Solution of printAllCombinations

--- src/run/RunMe.java
+++ src/run/RunMe.java
@@ -1,10 +1,18 @@
 package run;
 
+import problems.PrintAllCombinations;
+
 public class RunMe {
 
 	public static void main(String[] args) {
 		long startingTime = System.nanoTime(); // 1,000,000,000 nano = 1 second
+		//reseting the efficiency counter
+		PrintAllCombinations.counter1 = 0;
 		//Run here the solution
+		String sequence = "123456789";
+		System.out.println("Finding all possible combination from the string: " + sequence);
+		PrintAllCombinations.try1FindAllPossible(new StringBuilder(), new StringBuilder(sequence));
+		System.out.println("Efficiency counter = " + PrintAllCombinations.counter1);
 		long endTime = ( System.nanoTime() - startingTime)/ 1_000_000;
 		System.out.println("It's took about " + endTime + " ms, and " + endTime / 1000 + " seconds");
 
